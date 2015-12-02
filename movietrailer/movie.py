fb93ef0e7ac49e76d94415ba58a9bc4f0a77061d
robinJo <robin.joganah@gmail.com>
2015-12-01 22:10:20 -0500

    updated error typo
    updated error typo

--- movietrailer/movie.py
+++ movietrailer/movie.py
@@ -1,8 +1,7 @@
 class Movie:
-	""" This class is used to store data from movies such as the title,
-	the trailer URL """
-    def __init__(self, movie_title, movie_storyline, movie_poster,\
-     movie_trailer):
+    """ This class is used to store data from movies such as the title,
+    the trailer URL """
+    def __init__(self, movie_title, movie_storyline, movie_poster,movie_trailer):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
