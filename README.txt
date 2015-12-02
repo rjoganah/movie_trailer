7724925e7b9d8834d9ff62c0b6fae2a3b7dfbedb
robinJo <robin.joganah@gmail.com>
2015-12-01 21:50:30 -0500

    Movie_trailer
    Dynamic generation of website from python

---
+++ README.txt
@@ -0,0 +1,5 @@
+###MOVIE TRAILER PROJECT###
+
+You need to have Python installed in order to run the project.
+The command is the following : python movietrailer/main.py
+This command will generate the webpage fresh_tomatoes.html which will contain the trailers/posters of the movies.
---
+++ movietrailer/movie.py
@@ -0,0 +1,10 @@
+class Movie:
+	""" This class is used to store data from movies such as the title,
+	the trailer URL """
+    def __init__(self, movie_title, movie_storyline, movie_poster,\
+     movie_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
+
